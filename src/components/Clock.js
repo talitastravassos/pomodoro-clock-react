a3bd971d6fdc370fac974e1866645e1572a0269e
talitast <talitatravassos.tt@gmail.com>
2019-05-30 10:59:55 -0300

    added countdown

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,16 +1,57 @@
 
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { FaPlayCircle, FaRegPauseCircle, FaRedoAlt } from "react-icons/fa";
 import '../App.css';
 
 const Clock = (props) => {
 
     const [sessionLength, setSessionLength] = useState(25);
-
+    
     const [breakLength, setBreakLength] = useState(5);
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+    const [isRunning, setIsRunning] = useState(false)
 
-    useEffect(() => {
+    const formatTime = () => {
+        let min = minutes.toString().length === 1 ? "0" + minutes : minutes;
+        let sec = seconds.toString().length === 1 ? "0" + seconds : seconds;
+         return min + ':' + sec;
+      }
+
+
+
+    const countdown = () => {
+
+        let counter = sessionLength * 60;
+
+        let interval = setInterval(() => {
+
+                counter--;
+                setIsRunning(true)
+                let minutes = Math.floor(counter/60)
+                let seconds = counter % 60
+                setMinutes(minutes)
+                setSeconds(seconds)
+                
+                if(counter < 0 ){
+                    
+                    clearInterval(interval);
+                    console.log('Ding!');
+                };
         
-    }, [])
+            }, 1000);
+            //console.log(counter);
+            
+        
+      
+    }
+
+    // useEffect(() => {
+    //     formatTime(sessionLength)
+
+    //     //countdown(sessionLength);
+        
+    // }, [])
 
 
     return (
@@ -38,7 +79,16 @@ const Clock = (props) => {
 
             <div className='clock' id='grad'>
                 <p className='title'>Session</p>
-                <p className='timer'>25:00</p>
+                <p className='timer'>{formatTime()}</p>
+                <div className="timer-control">
+                    <button id="start_stop" onClick={countdown}>
+                        <FaPlayCircle/>
+                        <FaRegPauseCircle/>
+                    </button>
+                    <button id="reset">
+                        <FaRedoAlt/>
+                    </button>
+                </div>
             </div>
            
         </div>
